8251a0623d5a3e8c83e02f4f0c03fe6490f4077f
Maximiliano Jaque <maxi.jaque@gmail.com>
2022-01-23 09:52:21 -0300

    Se agrego JS (modal, tooltips, scrollsmooth, scrollnav) y se finalizo la página

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+$(function () {
+
+
+    $("a").click(function () {
+
+        var hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        },
+            200
+        )
+
+    });
+});
+
+$(document).scroll(function () {
+    const pixel = $("html").scrollTop()
+
+    if (pixel > 300) {
+        $("nav").addClass("nav-black")
+    } else {
+        $("nav").removeClass("nav-black")
+    }
+
+})
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
+
